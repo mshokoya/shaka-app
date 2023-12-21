87d6824a18488b6a172f52959a7f7500c7119075
msho <mayo_s@hotmail.co.uk>
2023-12-21 04:49:31 +0000

    layout started

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
+import { enableReactTracking } from "@legendapp/state/config/enableReactTracking";
+import { enableReactComponents } from "@legendapp/state/config/enableReactComponents"
+
+enableReactComponents()
+enableReactTracking({ auto: true })
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
