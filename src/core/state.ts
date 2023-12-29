e436bb79dbc30e33712df2145cb2d7a47093a5fb
msho <mayo_s@hotmail.co.uk>
2023-12-29 05:12:23 +0000

    working on state

--- src/core/state.ts
+++ src/core/state.ts
@@ -3,68 +3,51 @@ import {observable} from '@legendapp/state';
 
 export const state$ = observable({
     connection: {
-      mongodb: {
+      mongo: {
         "testconnkey1": {name: "test app", url: "mongodb://127.0.0.1:27017", host: '', port: '', user: '', password: 'password', database: '', configPath: '', key: 'testkey1'},
         "testconnkey2": {name: "test app", url: "mongodb://127.0.0.1:500000", host: '', port: '', user: '', password: 'passwordddd', database: '', configPath: '', key: 'testkey2'}
       },
     },
-    module: {
-      mongodb: {
-        testconnkey1: {
-          dbs: {
-            dbt1: {
-              tables: {
-                coltest1: { fields: ["testcol1", "testcol2", "testcol3"], data: [{testcol1: "hello", testcol2: "world", testcol3: "wee"}] },
-                coltest2: { fields: ["testcol4", "testcol5", "testcol6"], data: [{testcol4: "my", testcol5: "name", testcol6: "is"}] },
-                coltest3: { fields: ["testcol7", "testcol8", "testcol9"], data: [{testcol7: "zee", testcol8: "gee", testcol9: "imesa"}] },
-              }
-            },
-            dbt2: {
-              tables: {
-                coltest4: { fields: ["testcol1", "testcol2", "testcol3"], data: [{testcol1: "hdsadsao", testcol2: "wdsadasorld", testcol3: "wdsadee"}] },
-                coltest5: { fields: ["testcol4", "testcol5", "testcol6"], data: [{testcol4: "hevcxllo", testcol5: "worldavd", testcol6: "wevsdavdase"}] },
-                coltest6: { fields: ["testcol7", "testcol8", "testcol9"], data: [{testcol7: "hecsdfllo", testcol8: "wordvczxld", testcol9: "wecsxvcze"}] },
-              }
-            },
-          }
-        },
-        testconnkey2: {
-          dbs: {
-            dbt3: {
-              tables: {
-                coltest7:  {  fields: ["testcol10", "testcol11", "testcol12"], data: [{testcol10: "helsdavdsvslo", testcol11: "world", testcol12: "fdsafwee"}] },
-                coltest8:  {  fields: ["testcol13", "testcol14", "testcol15"], data: [{testcol13: "hdsvasdvello", testcol14: "world", testcol15: "vxzcwee"}] },
-                coltest9:  {  fields: ["testcol16", "testcol17", "testcol18"], data: [{testcol16: "dsadsds", testcol17: "fdsfzxc", testcol18: "vczxwfdsaf"}] },
-              }
-            },
-            dbt4: {
-              tables: {
-                coltest10: { fields: ["testcol1", "testcol2", "testcol3"], data: [{testcol1: "hvczvxcello", testcol2: "world", testcol3: "wee"}] },
-                coltest11: { fields: ["testcol4", "testcol5", "testcol6"], data: [{testcol4: "fgjhdhello", testcol5: "rwtsfgdworld", testcol6: "cxvfwee"}] },
-                coltest12: { fields: ["testcol7", "testcol8", "testcol9"], data: [{testcol7: "dgnfbhellovdf", testcol8: "wdsforlddf", testcol9: "wbsdbsee"}] },
-              }
-            },
-          }
-        }
-      }
-    },
-    display: {
-      layouts: {
-        default: {
-          mongodb: {
-            tree: ''
+    data:{
+      randomGenInDisplay: { key: '', type: 'db', display: 'customLayout1', module: 'mongo', connKey: 'testkey1', db: 'dbt1', table: 'coltest1', fields: ["testcol1", "testcol2", "testcol3"], data: [{testcol1: "hello", testcol2: "world", testcol3: "wee"}] },
+      mongo: { 
+        testkey1: {
+          dbt1: {
+            name: "dbt1",
+            tables: ["coltest1", "coltest2", "coltest3"],
           },
-          postgres: {
-            tree: ''
-          }
+          dbt2: {
+            name: "dbt2",
+            tables: ["coltest4", "coltest5", "coltest6"],
+          },
         },
-        customLayout1: {
-
+        testkey2: {
+          dbt3: {
+            name: "dbt3",
+            tables: ["coltest7", "coltest8", "coltest9"],
+          },
+          dbt4: {
+            name: "dbt4",
+            tables: ["coltest10", "coltest11", "coltest12"],
+          },
         }
-
-
       },
-      state:{}
+      randomTreeForMongo: { group: 'mongo', data: ''},
+      randomTreeIDForPOSTGRES: {group: 'postgres', data: ''},
+    },
+    
+    display: {
+      mongo: {
+        tree: 'randomTreeForID' // for default module all keys & group will be the given module name
+      },
+      postgres: {
+        tree: 'randomTreeIDForPOSTGRES'
+      },
+      customLayout1: {
+        layout: [
+          { group: 'rangroup1', name: 'display name',  data: 'randomGenInDisplay', i: "blue-eyes-dragon", x: 0, y: 0, w: 300, h: 50, isDraggable: true, resizeHandles: ['s' , 'w' , 'e' , 'n' , 'sw' , 'nw' , 'se' , 'ne']},
+        ]
+      },
     }
   })
 
@@ -173,3 +156,46 @@ export const state$ = observable({
   //   },
   //   moduleConfig: {}
   // }
+
+
+
+
+
+
+
+
+  // modules: {
+  //   mongo: {
+  //     keys: ["testkey1","testkey2"], // use computed() 
+  //     dbs: {
+  //       testkey1: {
+  //         dbt1: {
+  //           name: "dbt1",
+  //           tables: ["coltest1", "coltest2", "coltest3"],
+  //         },
+  //         dbt2: {
+  //           name: "dbt2",
+  //           tables: ["coltest4", "coltest5", "coltest6"],
+  //         },
+  //       },
+  //       testkey2: {
+  //         dbt3: {
+  //           name: "dbt3",
+  //           tables: ["coltest7", "coltest8", "coltest9"],
+  //         },
+  //         dbt4: {
+  //           name: "dbt4",
+  //           tables: ["coltest10", "coltest11", "coltest12"],
+  //         },
+  //       }
+  //     },
+  //     dbList: {
+  //       testkey1: ['dbt1', 'dbt2'], // use computed() + find a func that updates keys when added or removed
+  //       testkey2: ['dbt3', 'dbt4'],
+  //     },
+  //   },
+  //   data: {
+  //     randomGenInDisplay: { displayKey: '', module: 'mongo', connKey: 'testkey2', table: 'coltest1', type: 'db', fields: ["testcol1", "testcol2", "testcol3"], data: [{testcol1: "hello", testcol2: "world", testcol3: "wee"}] },
+  //   },
+
+  // },
